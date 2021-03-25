59fe184f5c5904dda425f309bd5bf99450595b54
lucenta <lucentiniandrew@gmail.com>
2021-03-25 11:00:59 -0400

    Hide key

--- src/wordladders/wordladders/settings.py
+++ src/wordladders/wordladders/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = config("SECRET_KEY") 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -19,8 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '&ws8t_ycbj7e20+hq33&la7i3+@k)i0q3ovvgv_p*$uhftqh&*'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
